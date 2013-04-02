a09a52fdd944ee3a8f6f85c8fee97d3b5dc4c5ae
Terry Hanks <terry@Noctus.thstudio.org>
2013-04-02 14:46:45 +0800

    support new producer for v0.8

--- produce_test.js
+++ produce_test.js
@@ -1,15 +1,13 @@
 var Producer = require('./lib/Producer');
 
-var producer = new Producer('social', {host : 'localhost'});
+var producer = new Producer('test', {host : '10.75.15.235'});
 producer.connect()
 producer.on('error', function(err){
   console.log("error: ", err);
 });
 
 console.log("producing for ", producer.topic);
-setInterval(function(){
 console.log("sending...");
-  producer.send('{"thisisa" : "test' + new Date() + '"}', function(err){
-    console.log("send error: ", err);
-  });
-}, 1000);
+producer.send('123', function(err){
+  console.log("send error: ", err);
+});
