57b076baaf71ec8ceb992e73e657a5217e33d1a4
Terry Hanks <terry@Noctus.thstudio.org>
2013-04-08 17:10:05 +0800

    full functional producer 	modified:   lib/Message.js 	new file:   lib/MessageSet.js 	modified:   lib/ProduceRequest.js 	new file:   lib/ProduceResponse.js 	modified:   lib/Producer.js 	modified:   lib/Response.js 	modified:   produce_test.js

--- produce_test.js
+++ produce_test.js
@@ -1,6 +1,6 @@
 var Producer = require('./lib/Producer');
 
-var producer = new Producer('test', {host : '10.75.15.235'});
+var producer = new Producer('test', {host : '10.75.15.235', 'requireAcks': 1});
 producer.connect()
 producer.on('error', function(err){
   console.log("error: ", err);
@@ -8,6 +8,8 @@ producer.on('error', function(err){
 
 console.log("producing for ", producer.topic);
 console.log("sending...");
-producer.send('123', function(err){
-  console.log("send error: ", err);
+producer.send(['123','456','789'], function(err, res){
+  console.log("Error: ", err);
+  console.log("Response: ", JSON.stringify(res));
+  producer.connection.end();
 });
