4d627d3144dee8ce79cbfa6fef4b8e50a2b665de
Terry Hanks <terry@Noctus.thstudio.org>
2013-04-12 15:16:22 +0800

    add fetch api 	modified:   consume_test.js 	modified:   lib/Consumer.js 	modified:   lib/FetchRequest.js 	modified:   lib/FetchResponse.js 	modified:   lib/Message.js 	modified:   lib/MessageSet.js 	modified:   lib/ProduceRequest.js 	modified:   lib/Request.js 	modified:   lib/Response.js

--- consume_test.js
+++ consume_test.js
@@ -1,6 +1,6 @@
 var Consumer = require('./lib/Consumer');
 
-var options = {host : 'xtnvkafka01.xt.local', topic : 'social', partition : 0};
+var options = {host : '10.75.15.235', topic : 'test', partition : 0};
 options = {host : 'localhost', topic : 'social', partition : 0, offset : 0};
 var consumer = new Consumer(options);
 consumer.connect(function(err){
