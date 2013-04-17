942b281424179a5f0eb408b335974e1d3d7e8b76
Terry Hanks <terry@Noctus.thstudio.org>
2013-04-17 15:46:32 +0800

    add Client interface 	modified:   consume_test.js 	new file:   lib/Client.js 	modified:   lib/Consumer.js 	modified:   lib/MessageSet.js 	new file:   lib/Metadata.js

--- consume_test.js
+++ consume_test.js
@@ -1,24 +1,26 @@
 var Consumer = require('./lib/Consumer');
 
 
-options = {host : '10.75.15.234', topic : 'topic', partition : 0, offset : 0};
+options = {host : '10.75.15.235', topic : 'topic', partition : 0, offset : 0};
 var consumer = new Consumer(options);
 consumer.connect(function(err){
   if (err){
     console.log("Error: " + err);
     throw "could not connect to Kafka";
   }
-  console.log("connected!!");
-
-  console.log("===================================================================");
-  console.log(new Date());
+  console.log("connected!!");  
   console.log("consuming: " + consumer.topic);
+  
   consumer.on('message', function(messages){
     messages.forEach(function(m){
       console.log(m.payload.toString());
     });
     setTimeout(consumer.consume(),2000);
   });
+  consumer.on('error', function(error){
+    console.log(error);
+  });
+
   consumer.consume();
     
 });
